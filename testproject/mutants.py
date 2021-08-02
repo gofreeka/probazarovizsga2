27a6b6ad5feac117118622ea5e07f48fdf73874f
gofreeka <gabormartha.mail@gmail.com>
2021-08-02 23:56:01 +0200

    mutants.py update

--- testproject/mutants.py
+++ testproject/mutants.py
@@ -21,5 +21,83 @@ try:
     assert hellfire_club_team.is_displayed()
 
 
+    # Original Team:
+
+
+    def og_team():
+        original_team.click()
+        time.sleep(1)
+
+        # Team members list
+        team_members_id = driver.find_elements_by_xpath("//li[@id]/h2")
+        print(">>> Original X-men Team members: ")
+        for cr in team_members_id:
+
+            # Print if displayed
+            if cr.is_displayed():
+                print(cr.text)
+
+
+    # X-Force Team:
+
+
+    def force_team():
+        x_force_team.click()
+        time.sleep(1)
+
+        # Team members list
+        team_members_id = driver.find_elements_by_xpath("//li[@id]/h2")
+        print(">>> X-Force Team members: ")
+        for cr in team_members_id:
+
+            # Print if displayed
+            if cr.is_displayed():
+                print(cr.text)
+
+
+    # X-Factor Team:
+
+
+    def factor_team():
+        x_factor_team.click()
+        time.sleep(1)
+
+        # Team members list
+        team_members_id = driver.find_elements_by_xpath("//li[@id]/h2")
+        print(">>> X-Factor Team members: ")
+        for cr in team_members_id:
+
+            # Print if displayed
+            if cr.is_displayed():
+                print(cr.text)
+
+
+    # Hellfire Team:
+
+
+    def hellfire_team():
+        hellfire_club_team.click()
+        time.sleep(1)
+
+        # Team members list
+        team_members_id = driver.find_elements_by_xpath("//li[@id]/h2")
+        print(">>> Hellfire Club members: ")
+        for cr in team_members_id:
+
+            # Print if displayed
+            if cr.is_displayed():
+                print(cr.text)
+
+
+    og_team()
+    force_team()
+    factor_team()
+    hellfire_team()
+
+    # A tagok csapatban valo ellenorzeset a data-teams attributummal oldanam meg,
+    # abban az iranyban a keresem a megoldast
+
+    # d_teams = driver.find_elements_by_xpath("//li[@data-teams]")
+
 finally:
     driver.quit()
